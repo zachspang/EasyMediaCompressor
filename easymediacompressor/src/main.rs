8de42a212629927595a97da05d5940445fdcf7a9
zachspang <spangzach@gmail.com>
2024-04-06 02:13:22 -0700

    combine button logic into the ButtonLogic singleton and implement the file size unit drop down box

--- easymediacompressor/src/main.rs
+++ easymediacompressor/src/main.rs
@@ -3,17 +3,11 @@ use cmd_lib::*;
 use rfd::FileDialog;
 use slint::SharedString;
 /*Notes
-Compress input.mp4 to target size
-ffmpeg -i input.mp4 -b:v 555KiB -bufsize 555KiB output.mp4
-
-Get duration of input.mp4
-ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 input.mp4
-
 How to run: 
-cargo run -- C:\path\input.mp4 C:\path\output.mp4 targetSizeInMB
+cd easymediacompressor
+cargo run
 
 TODO: Setting menu with config file, Move gui into a .slint file,  Compression for Audio & Images, File type conversions
-
 */
 
 slint::slint!{
@@ -27,9 +21,12 @@ slint::slint!{
         DefaultInput,
         DefaultOutput,
     }
-    export global ChooseFileLogic{
-        callback button-pressed(ChooseFileButtonType);
+
+    export global ButtonLogic{
+        callback choose-file-button-pressed(ChooseFileButtonType);
+        callback compress-button-pressed();
     }
+    
     component ChooseFile {
         in property <ChooseFileButtonType> type;
         in-out property <bool> enabled <=> ta.enabled;
@@ -42,7 +39,7 @@ slint::slint!{
             border-radius: 10px;
             border-color: self.background.darker(20%);
             ta := TouchArea{
-                clicked => {ChooseFileLogic.button-pressed(root.type);}
+                clicked => {ButtonLogic.choose-file-button-pressed(root.type);}
             }
             states [
                 active when enabled: {
@@ -56,9 +53,6 @@ slint::slint!{
         Text{ text: "Choose File";}
     }
 
-    export global CompressButtonLogic {
-        callback button-pressed();
-    }
     component CompressButton {
         in-out property <bool> enabled <=> ta.enabled;
         Rectangle {
@@ -71,7 +65,7 @@ slint::slint!{
             border-color: self.background.darker(20%);
             ta := TouchArea{
                 clicked => {
-                    CompressButtonLogic.button-pressed();
+                    ButtonLogic.compress-button-pressed();
                 }
             }
             states [
@@ -93,6 +87,8 @@ slint::slint!{
         in property <string> input_path;
         in property <string> output_path;
         in-out property <int> target_size;
+        in-out property <string> size_unit: "MB";
+
         width: 420px;
         height: 330px;
         background: #272626;
@@ -122,8 +118,8 @@ slint::slint!{
                     horizontal-alignment: left;
                     //Keep target size less than 1000, anything else is proably unintentional or should be a different unit
                     edited => {
-                        if self.text.to-float() > 1000{
-                            self.text = 1000;
+                        if self.text.to-float() > 9999{
+                            self.text = 9999;
                         }
                         target_size = self.text.to-float();
                     }
@@ -134,7 +130,10 @@ slint::slint!{
                     enabled: widgets-enabled;
                     width:70px;
                     height: 30px;
-                    model: ["MB", "KB", "GB"];
+                    model: ["MB","GB"];
+                    selected => {
+                        size_unit = self.current-value;
+                    }
                 } 
                 Text {
                     color: white;
@@ -241,7 +240,7 @@ fn main() {
     let app = App::new().unwrap();
 
     //Opens system file dialog to select a file path
-    app.global::<ChooseFileLogic>().on_button_pressed({
+    app.global::<ButtonLogic>().on_choose_file_button_pressed({
         let weak = app.as_weak();
         move |value|{
             let app = weak.unwrap();
@@ -271,13 +270,14 @@ fn main() {
 
     //Calls compress_video when the compress button is pressed. 
     //compress_video is called on a new thread so the gui still responds while video is compressing
-    app.global::<CompressButtonLogic>().on_button_pressed({
+    app.global::<ButtonLogic>().on_compress_button_pressed({
         let weak = app.as_weak();
         move ||{
             let app = weak.unwrap();
             let input_path = app.get_input_path().to_string();
             let output_path = app.get_output_path().to_string();
             let target_size = app.get_target_size() as f32;
+            let size_unit = app.get_size_unit();
             
             //Stop widgets from working when video is compressing and clear previous compress result
             app.set_widgets_enabled(false);
@@ -287,7 +287,7 @@ fn main() {
             let weak = app.as_weak();
             thread::spawn( move ||{
                 let weak_copy = weak.clone();
-                let compress_result = compress_video(input_path,output_path,target_size);
+                let compress_result = compress_video(input_path,output_path,target_size, size_unit.to_string());
                 let string_result;
 
                 match compress_result {
@@ -309,7 +309,7 @@ fn main() {
     
 }
 
-fn compress_video(input_path: String, output_path: String, target_size: f32) -> Result<(), std::io::Error> {
+fn compress_video(input_path: String, output_path: String, mut target_size: f32, size_unit: String) -> Result<(), std::io::Error> {
     /*
     TODO: Make way to compress without opening gui by dragging and dropping a file onto the exe
     let args: Vec<String> = env::args().collect();
@@ -332,6 +332,18 @@ fn compress_video(input_path: String, output_path: String, target_size: f32) -> 
         return Err(Error::new(io::ErrorKind::InvalidInput, "The output path doesn't exist"));
     }
 
+    if target_size > 9999 as f32 || target_size < 1 as f32{
+        return Err(Error::new(io::ErrorKind::InvalidInput, "Invalid target size"));
+    }
+
+    //Turn target size into kB
+    match size_unit.as_str(){
+        "MB" => target_size *= 1024 as f32,
+        "GB" => target_size *= 1048576 as f32,
+        _ => {}
+    }
+    dbg!(size_unit);
+    dbg!(target_size);
     //create output file name, if it already exists add (i)
     let mut output_file = format!("{}{}", &output_path, "\\output.mp4");
     let mut file_number = 1;
@@ -340,34 +352,50 @@ fn compress_video(input_path: String, output_path: String, target_size: f32) -> 
         file_number += 1;
     }
 
-    if target_size > 1000 as f32 || target_size < 1 as f32{
-        return Err(Error::new(io::ErrorKind::InvalidInput, "Invalid target size"));
-    }
-    
     //timer is used to find total time elapsed
     let timer = Instant::now();
 
     //TODO: Error handling if one of these doesnt return
     let duration: f32 = run_fun!(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 $input_path).unwrap().trim().parse().expect("Invalid duration");
     let video_bitrate: f32 = run_fun!(ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 $input_path).unwrap().trim().parse().expect("Invalid video bitrate");
-    let audio_bitrate: f32 = run_fun!(ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 $input_path).unwrap().trim().parse().expect("Invalid audio bitrate");
   
     println!("duration = {} seconds", duration);
 
-    //Calculate new bitrate in KB to reach target file size
-    let new_bitrate = ((target_size * 1024 as f32) / duration) - (audio_bitrate / 8192 as f32);
-    println!("old_bitrate = {} KB/s", video_bitrate / 8192 as f32);
-    println!("new_bitrate = {} KB/s", new_bitrate);
+    //Calculate new bitrate in kB/s with space for 16 kB/s for audio to reach target file size 
+    let mut new_video_bitrate = (target_size / duration) - 16 as f32;
+
+    println!("old_bitrate = {} kB/s", video_bitrate / 8192 as f32);
+    println!("new_bitrate = {} kB/s", new_video_bitrate);
     
     //Make sure the new bitrate is lower than the old bitrate
-    if new_bitrate > (video_bitrate / 8192 as f32){
+    if new_video_bitrate > (video_bitrate / 8192 as f32){
         return Err(Error::new(io::ErrorKind::InvalidInput, "The video's current filesize is too close to the target. Please try a smaller target."));
     }
 
+    //If the user inputs a really small size the bitrate can end up negative since we leave 16kB/s for audio.
+    if new_video_bitrate < 1.0 {
+        new_video_bitrate = 1.0;
+    }
+    
     //This runs ffmpeg to lower the videos bitrate//TODO: Currently "-y" force overwrites any file with the same name as output_file, add check that path is empty, add (1), (2) etc at the end otherwise.
-    let compress_status = run_cmd!(ffmpeg -v error -i $input_path -b:v ${new_bitrate}KiB -bufsize ${new_bitrate}KiB $output_file);
-
+    let compress_status = run_cmd!(ffmpeg -v error -i $input_path -b:v ${new_video_bitrate}KiB -bufsize ${new_video_bitrate}KiB $output_file);
+    
+    //Slower but better quality two pass encoding to compress video
+    //TODO: Add option to enable this
+    //TODO:Add check for operating system and change NUL to /dev/null for Unix based systems
+    //let pass1 = run_cmd!(ffmpeg -y -i $input_path -c:v libx265 -b:v ${new_video_bitrate}KiB -x265-params pass=1 -f null NUL);
+    //let pass2 = run_cmd!(ffmpeg -i $input_path -c:v libx265 -b:v ${new_video_bitrate}KiB -x265-params pass=2 -c:a aac -b:a 128k $output_file);
+    
     println!("Total Time Elapsed: {}ms", timer.elapsed().as_millis());
     return compress_status;
+
+
+    //returns for two pass encoding
+    // if pass1.is_err(){
+    //     return pass1;
+    // }
+    // else{
+    //     return pass2;
+    // }
 
 }
